419116018ea3e4c5bd34ae2dce39e0ec777c2ee5
NicJcoding <nijcoding@gmail.com>
2023-04-25 23:37:27 -0500

    Updated through 18

--- problems/17-xor-select.js
+++ problems/17-xor-select.js
@@ -33,12 +33,52 @@ console.log(
 
 let xorSelect = function(array, cb1, cb2) {
   // Your code here
-  
+  let newArr = [];
   array.forEach(element => {
+    let res1 = cb1(element); // true or false
+    let res2 = cb2(element); // true or false
+
+    if ((res1 === true || res2 === true) && !(res1 === true && res2 === true)) {
+      newArr.push(element);
+    }
+
+  });
+  
+return newArr;
+};
+
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function(n) {
+  return n > 0;
+};
+
+console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// [ -2, 1, 3 ]
+
+
+let longString = function(s) {
+  return s.length > 4;
+};
+
+let startsA = function(s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+
+// [ 'art', 'app', 'buttery' ]
+
+
+/*   array.forEach(element => {
     let res = cb1(element);
     let res2 = cb2(element);
   });
-};
+  */
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
